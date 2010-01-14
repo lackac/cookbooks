b116d749c729307988464e6f84e6670efb456006
Laszlo Bacsi <lackac@lackac.hu>
2010-01-14 03:10:11 +0100

    database resource with create/delete actions

---
+++ mysql/resources/default.rb
@@ -0,0 +1,6 @@
+actions :create, :delete
+
+attribute :name, :kind_of => String, :name_attribute => true
+attribute :user, :kind_of => String
+attribute :password, :kind_of => String
+attribute :host, :kind_of => String
