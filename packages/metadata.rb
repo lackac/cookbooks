45942e2392489b6841f458bfdcf15f8d4dd50233
Laszlo Bacsi <lackac@lackac.hu>
2010-01-24 10:42:37 +0100

    Install extra packages

--- packages/metadata.rb
+++ packages/metadata.rb
@@ -1,8 +1,8 @@
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
-description       "Helper library to determine whether distribution-only packages are installed"
-version           "0.8"
+description       "Helper library to determine whether distribution-only packages are installed and to install/remove extra packages"
+version           "0.9"
 
 %w{redhat centos}.each do |os|
   supports os
@@ -18,3 +18,7 @@ attribute "packages/dist_only",
   :description => "Set to only use distribution-provided packages",
   :default => "false"
 
+attribute "packages/packages",
+  :display_name => "Extra packages to install/remove",
+  :description => "Hash of packages in form of 'package_name' => {attributes}",
+  :type => "hash"
